a72b4fa780e54cb2ad10e8972b1493157d0dd0f7
Hector <orantesroman39@gmail.com>
2024-12-05 23:15:57 -0600

    update characters

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CharactersModule } from "./characters/characters.module";
+import { PlanetsModule } from "./planet/planets.module";
 
 @NgModule({
   declarations: [
     AppComponent,
- 
 
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
-  ],
+    AppRoutingModule,
+    CharactersModule,
+    HttpClientModule,
+    PlanetsModule
+],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/app/service/shared-service.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { Character } from '../characters/interface/characters';
+@Injectable({
+  providedIn: 'root'
+})
+export class SharedService {
+  private filteredCharactersSource = new BehaviorSubject<Character[]>([]);
+  filteredCharacters$ = this.filteredCharactersSource.asObservable();
+
+  setFilteredCharacters(characters: Character[]): void {
+    console.log('Enviando personajes filtrados al BehaviorSubject:', characters);
+    this.filteredCharactersSource.next(characters);
+  }
+}

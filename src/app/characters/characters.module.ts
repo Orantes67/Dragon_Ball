7c89abf58234cc525340485822bd066406212619
Hector <orantesroman39@gmail.com>
2024-12-05 21:22:35 -0600

    charecters

---
+++ src/app/characters/characters.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CardsCharactersComponent } from './cards-characters/cards-characters.component';
+@NgModule({
+  declarations: [
+    CardsCharactersComponent
+  ],
+  imports: [
+    CommonModule
+  ],exports:[
+    CardsCharactersComponent
+  ]
+})
+export class CharactersModule { }
